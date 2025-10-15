9a01b91cb4642b480a7ff593687061c82c2b8214
Smilla Sollén <ss226uk@student.lnu.se>
2025-10-15 12:46:11 +0200

    Save largetext and level attributes in mongodb

--- src/models/Palette.js
+++ src/models/Palette.js
@@ -10,6 +10,8 @@ const paletteSchema = new mongoose.Schema({
   name: { type: String, required: true },
   basecolor: { type: String, required: true },
   colors: { type: mongoose.Schema.Types.Mixed, required: true },
+  level: { type: String, enum: ['AA', 'AAA'], required: true },
+  isLargeText: { type: Boolean, required: true },
   createdAt: { type: Date, default: Date.now }
 })
 
