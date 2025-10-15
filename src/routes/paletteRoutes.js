c00cd2cc6839490ceebfefa01ca4aa8a59076e9e
Smilla Sollén <ss226uk@student.lnu.se>
2025-10-15 12:35:38 +0200

    Route for showing/getting saved palettes

--- src/routes/paletteRoutes.js
+++ src/routes/paletteRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const paletteController = new PaletteController()
 
 router.post('/save', (req, res) => paletteController.savePalette(req, res))
+router.get('/', (req, res) => paletteController.showAllPalettes(req, res));
 
 export default router
