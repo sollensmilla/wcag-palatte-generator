8afdef22c7ad4b58617c07ea03f959877b7b7b57
Smilla Sollén <ss226uk@student.lnu.se>
2025-10-17 15:23:47 +0200

    Cleanup index.js

--- index.js
+++ index.js
@@ -4,44 +4,51 @@
  * @author Smilla Sollén <ss226uk@student.lnu.se>
  * @version 1.0.0
  */
+import dotenv from 'dotenv';
+import express from 'express'
 import bodyParser from 'body-parser'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
-import { getHomePage } from './src/controllers/mainController.js'
-
-import dotenv from 'dotenv';
-dotenv.config();
-
 import mongoose from 'mongoose'
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err))
 
-import express from 'express'
-const app = express()
-const PORT = process.env.PORT || 3015
-
+import getHomePage from './src/controllers/mainController.js'
 import PaletteController from './src/controllers/paletteController.js'
-const paletteController = new PaletteController()
 
+import paletteRoutes from './src/routes/paletteRoutes.js'
+
+import configureFlash from './src/middleware/configure-flash.js'
+import { errorHandler } from './src/middleware/error-handler.js'
+
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+const app = express()
+const PORT = process.env.PORT || 3015
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.error('MongoDB connection error:', err)
+    process.exit(1) 
+  }
+}
+connectToDatabase()
+
 app.set('views', join(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
+
 app.use(express.static(join(__dirname, 'src', 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
-
 configureFlash(app);
 
-import paletteRoutes from './src/routes/paletteRoutes.js'
+const paletteController = new PaletteController()
 app.use('/palette', paletteRoutes)
 app.get('/', getHomePage)
 
-import { errorHandler } from './src/middleware/error-handler.js';
 app.use(errorHandler);
-
-import configureFlash from './src/middleware/configure-flash.js';
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
