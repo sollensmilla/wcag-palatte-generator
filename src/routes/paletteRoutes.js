038e3ea0064aa9f2f73b9a6a0b039129f0303753
Smilla Sollén <ss226uk@student.lnu.se>
2025-10-20 11:39:36 +0200

    Fix import

--- src/routes/paletteRoutes.js
+++ src/routes/paletteRoutes.js
@@ -7,9 +7,9 @@ const router = express.Router()
 const paletteController = new PaletteController()
 const validator = new PaletteValidator()
 
-router.get('/', (req, res) => paletteController.showAllPalettes(req, res));
-router.post('/generate', (req, res, next) => validator.validate(req, res, next), (req, res) => paletteController.generatePalette(req, res));
-router.post('/save', (req, res) => paletteController.savePalette(req, res));
-router.post('/delete/:id', (req, res) => paletteController.deletePalette(req, res));
+router.get('/', (req, res, next) => paletteController.showAllPalettes(req, res, next));
+router.post('/generate', (req, res, next) => validator.validate(req, res, next), (req, res, next) => paletteController.generatePalette(req, res, next));
+router.post('/save', (req, res, next) => paletteController.savePalette(req, res, next));
+router.post('/delete/:id', (req, res, next) => paletteController.deletePalette(req, res, next));
 
 export default router;
